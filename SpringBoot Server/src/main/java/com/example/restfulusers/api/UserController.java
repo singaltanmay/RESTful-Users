453dd1d2e600e96e1464833e7e96d92b1d910fa8
Tanmay Singal <tanmaysingal@icloud.com>
2019-12-25 15:57:23 +0530

    Added Api description to Swagger page

--- SpringBoot Server/src/main/java/com/example/restfulusers/api/UserController.java
+++ SpringBoot Server/src/main/java/com/example/restfulusers/api/UserController.java
@@ -1,6 +1,7 @@
 package com.example.restfulusers.api;
 
 import com.example.restfulusers.model.User;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,36 +21,44 @@ public class UserController {
         this.service = service;
     }
 
+    @ApiOperation(value = "Gets all users from the database.")
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
         return service.getAllUsers();
     }
 
+    @ApiOperation(value = "Gets the user having given ID from the database.")
+    /*@ApiImplicitParams(name = "ID", value = "UUID of the required user.")*/
     @GetMapping(path = "{id}")
     public ResponseEntity<User> getUserByID(@PathVariable("id") UUID uuid) {
         return service.getUserByID(uuid);
     }
 
+    @ApiOperation(value = "Adds a new user to the database having a randomly generated ID.")
     @PostMapping
     public ResponseEntity<?> insertNewUser(@RequestBody User user) {
         return service.insertNewUser(user);
     }
 
+    @ApiOperation(value = "Replaces pre-existing user at specified ID else it adds a new user at that ID.")
     @PutMapping(path = "{id}")
     public ResponseEntity<?> replaceOrInsertUserByID(@PathVariable("id") UUID uuid, @RequestBody User user) {
         return service.replaceOrInsertUserByID(uuid, user);
     }
 
+    @ApiOperation(value = "Modifies the user at specified ID with given data. Only provided values are modified and rest are retained.")
     @PatchMapping(path = "{id}")
     public ResponseEntity<?> updateUserByID(@PathVariable("id") UUID uuid, @RequestBody User user) {
         return service.updateUserByID(uuid, user);
     }
 
+    @ApiOperation("Deletes all users present in the database.")
     @DeleteMapping
     public ResponseEntity<?> deleteAllUsers() {
         return service.deleteAllUsers();
     }
 
+    @ApiOperation("Deletes the user present at specified ID.")
     @DeleteMapping(path = "{id}")
     public ResponseEntity<?> deleteUserByID(@PathVariable("id") UUID uuid) {
         return service.deleteUserByID(uuid);
